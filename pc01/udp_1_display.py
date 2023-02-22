f79d278611349b377f0e6836f41283ef1baff8a8
Kassen Boyaubay <kassen.boyaubay@nu.edu.kz>
2023-02-22 08:38:56 +0100

    init

---
+++ pc01/udp_1_display.py
@@ -0,0 +1,15 @@
+import socket
+
+#adresa a port prijemce (zada vyucujici)
+host = "147.229.150.101"
+port = 50000
+
+#vytvoreni socketu
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#zaslani zpravy
+n = udp_socket.sendto(b"Ahoj", (host, port))
+print("Odeslano {} byte\n".format(n))
+
+#uzavreni socketu
+udp_socket.close()
