cb5f64560d7ef91e7be97116402eef392ede64e4
Kassen Boyaubay <kassen.boyaubay@nu.edu.kz>
2023-02-22 09:08:48 +0100

    Odesílání binárních dat v „síťovém pořadí“: Network Order

--- pc01/udp_1_display.py
+++ pc01/udp_1_display.py
@@ -1,15 +1,27 @@
 import socket
+import struct
+import time
 
 #adresa a port prijemce (zada vyucujici)
-host = "147.229.150.101"
-port = 50000
+dst_host = "147.229.150.101"
+dst_port = 50000
 
 #vytvoreni socketu
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#zaslani zpravy
-n = udp_socket.sendto(b"Kassen", (host, port))
-print("Odeslano {} byte\n".format(n))
+for i in range(5):
+    seq = i    
+    n1 = 71
+    n2 = 10
+
+    #seq, n1 a n2 jsou prenasena cisla
+    msg = struct.pack("!LLL", seq, n1, n2)
+    
+    #zaslani zpravy
+    n = udp_socket.sendto(msg, (dst_host, dst_port))
+    print("Odeslano {} byte\n".format(n))
+    
+    time.sleep(0.5)
 
 #uzavreni socketu
 udp_socket.close()  
